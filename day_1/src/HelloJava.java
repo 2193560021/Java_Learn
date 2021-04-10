ee79f78109fc718c4477e496fc3a6eb678bb6b83
2193560021 <62364555+2193560021@users.noreply.github.com>
2021-04-10 13:43:49 +0800

    类与对象

--- day_1/src/HelloJava.java
+++ day_1/src/HelloJava.java
@@ -1,5 +1,16 @@
+class Book {        //定义一个类
+    String title;   //书名
+    double price;   //书的价格
+    public void getInfo(){  //此方法将由对象调用
+        System.out.println("图书名称：" + title + "，价格：" +price);
+    }
+}
+
 public class HelloJava {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        Book bk = new Book();   //声明并实例化对象
+        //bk.title = "Java开发";    //操作属性内容
+        //bk.price = 89.9;
+        bk.getInfo();
     }
 }
